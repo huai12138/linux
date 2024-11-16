ab5a5d937a5d0ae2d183a04a18ff7fca4ed3e977
huaihuaidehuaihuai <huaihuaidehuaihuai@gmail.com>
2024-11-16 14:28:47 +0800

    fix cpu_temp_monitor

--- cpu_temp_monitor.sh
+++ cpu_temp_monitor.sh
@@ -5,6 +5,6 @@ TEMPERATURE=$(cat /sys/class/thermal/thermal_zone0/temp | awk '{print $1/1000}')
 # 使用整数比较
 if [ "$TEMPERATURE" -gt "$THRESHOLD" ]; then
     MESSAGE="🔴 警告！TrueNas CPU温度达到了 $TEMPERATURE °C！"
-    curl -s -X POST "https://api.telegram.org/bot5644773960:AAGiZvIJVKSMjw9Hua5cQ05Ac7rTqJcRrhY/sendMessage" -d "chat_id=5407421728&text=$MESSAGE&parse_mode=HTML"
+    curl -s -X POST "https://api.telegram.org/bot5644773960:AAGiZvIJVKSMjw9Hua5cQ05Ac7rTqJcRrhY/sendMessage" -d "chat_id=-1001865599136&text=$MESSAGE&parse_mode=HTML"
 fi
 
