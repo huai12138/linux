3bc1af7051b07eb84ed46cddf98e77b33a84d3c9
huaihuaidehuaihuai <huaihuaidehuaihuai@gmail.com>
2024-11-20 13:55:11 +0800

    add github

---
+++ git_config.sh
@@ -0,0 +1,4 @@
+git config --global core.editor "vim"
+git config --global user.email "huaihuaidehuaihuai@gmail.com"
+git config --global user.name "huaihuaidehuaihuai"  
+git config --global credential.helper store
