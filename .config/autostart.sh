412eb4e0b691193e496cfa8b82b7baf08b72385c
huai12138 <huaihuaidehuaihuai@gmail.com>
2024-12-24 09:01:36 +0800

    fix

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -2,4 +2,7 @@
 
 google-chrome-stable &
 telegram-desktop &
-/bin/zsh ~/.config/wallpaperautochange.sh
+/bin/zsh ~/.config/wallpaperautochange.sh &
+/bin/zsh ~/.config/startwindows.sh 
+sleep 20 
+/bin/zsh ~/.config/windows.sh 
