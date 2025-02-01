211dd20c355b2a7758d3967878bd6d38811658e0
huai12138 <huaihuaidehuaihuai@gmail.com>
2025-02-02 00:16:53 +0800

    test

--- convert_crlf_to_lf.sh
+++ convert_crlf_to_lf.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# 使用 find 命令递归查找所有 .sh 文件
-find . -type f -name "*.sh" | while read file; do
-  # 使用 vim 将文件格式设置为 Unix 并保存
-  vim -E -s "$file" <<-EOF
-    :set ff=unix
-    :wq
-EOF
+# 使用 find 命令递归查找所有 .sh 文件并转换为 Unix 格式
+find . -type f -name "*.sh" | while read -r file; do
+  sed -i 's/\r$//' "$file"
 done
