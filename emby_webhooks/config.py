82b0a6884e63837d2985f7590169bf56a464f520
huai12138 <huaihuaidehuaihuai@gmail.com>
2025-02-09 00:37:48 +0800

    add

---
+++ emby_webhooks/config.py
@@ -0,0 +1,11 @@
+# Telegram 配置
+TELEGRAM_BOT_TOKEN = '' # 修改为你的实际 Bot Token
+TELEGRAM_CHAT_ID = ''  # 修改为你的实际 Chat ID
+
+# Emby 服务器配置
+SERVER_NAME = 'nas'  # 修改为你的实际服务器名称
+
+# Flask 应用配置
+FLASK_HOST = '0.0.0.0'
+FLASK_PORT = 5003
+DEBUG = True
