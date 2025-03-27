ee2ebb7493ac6697bdb7a5be1fb2f44173e8173d
huai12138 <huai12138@hotmail.com>
2025-03-27 14:54:13 +0800

    fix

--- .config/repo.sh
+++ .config/repo.sh
@@ -7,4 +7,5 @@ if ! command -v reflector &> /dev/null; then
 fi
 
 # 更新镜像列表
-sudo reflector --country China --age 12 --protocol https --sort rate --score 5 --save /etc/pacman.d/mirrorlist
+sudo reflector --country China --age 12 --protocol https --sort rate --score 3 --save /etc/pacman.d/mirrorlist
+cat /etc/pacman.d/mirrorlist
