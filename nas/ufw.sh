002c8b7ecd91c5402c3299cb333157617643cb19
huai12138 <huai12138@hotmail.com>
2025-03-28 19:00:54 +0800

    fix

--- nas/ufw.sh
+++ nas/ufw.sh
@@ -6,4 +6,9 @@ sudo ufw allow from 10.0.0.15 to any port 139,445 proto tcp
 sudo ufw allow from 10.0.0.8 to any port 137,138 proto udp
 sudo ufw allow from 10.0.0.8 to any port 139,445 proto tcp
 sudo ufw allow in on docker0
-
+sudo ufw allow from 10.0.0.25 to any port 2049 proto tcp  # NFS 默认端口
+sudo ufw allow from 10.0.0.25 to any port 20048 proto tcp  # mountd
+sudo ufw allow from 10.0.0.25 to any port 32765 proto tcp  # statd
+sudo ufw allow from 10.0.0.25 to any port 32766 proto tcp  # statd outgoing
+sudo ufw allow from 10.0.0.25 to any port 32767 proto tcp  # lockd
+sudo ufw allow from 10.0.0.25 to any port 32768 proto udp  # lockd
