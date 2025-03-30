e4ad02d892e62db9d17914302d150127f1dfb9a2
huai12138 <huai12138@hotmail.com>
2025-03-30 19:17:01 +0800

    fix

--- git_config.sh
+++ git_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git config --global core.editor "vim" # set vim as default editor
 git config --global user.email "@hotmail.com" # git config --global user.email "youremail"
-# git config --global user.name "huai12138"  # git config --global user.name "yourname"
+git config --global user.name "huai12138"  # git config --global user.name "yourname"
 git config --global credential.helper store # store password
 
 # echo "# ceshi" >> README.md
