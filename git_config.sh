60ada1362bbc99d5e77d62febfac0fec10ae8f41
huai12138 <110147911+huai12138@users.noreply.github.com>
2025-04-25 14:54:14 +0800

    Update git_config.sh

--- git_config.sh
+++ git_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git config --global core.editor "vim" # set vim as default editor
-git config --global user.email "@hotmail.com" # git config --global user.email "youremail"
+git config --global user.email "huai12138@hotmail.com" # git config --global user.email "youremail"
 git config --global user.name "huai12138"  # git config --global user.name "yourname"
 # git config --global credential.helper store # store password
 # 改为ssh方式pull push
