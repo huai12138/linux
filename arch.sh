de3ef9fc4be6ebbd051126a26c445a5cde55bde7
huai12138 <huai12138@hotmail.com>
2025-05-01 11:46:50 +0800

    improve

--- arch.sh
+++ arch.sh
@@ -1,151 +1,272 @@
 #!/bin/bash
 
-# 添加错误处理函数
-set -e  # 任何命令失败立即退出
+# Default values
+DISK="/dev/nvme0n1"
+HOSTNAME="huai"
+USERNAME="huai"
+SWAP_SIZE="16384M"
+PART_PREFIX=""  # Will be set based on device type
+UCODE=""        # Will be set based on CPU detection
+
+# Add help option
+usage() {
+  echo "Usage: $0 [options]"
+  echo "Options:"
+  echo "  -d DEVICE   Set installation disk (default: /dev/nvme0n1)"
+  echo "  -h NAME     Set hostname (default: huai)"
+  echo "  -u USER     Set username (default: huai)"
+  echo "  -s SIZE     Set swap size (default: 16384M)"
+  echo "  -?          Show this help message"
+  exit 0
+}
+
+# Parse command line arguments
+while getopts "d:h:u:s:?" opt; do
+  case $opt in
+    d) DISK="$OPTARG" ;;
+    h) HOSTNAME="$OPTARG" ;;
+    u) USERNAME="$OPTARG" ;;
+    s) SWAP_SIZE="$OPTARG" ;;
+    \?) usage ;;
+  esac
+done
+
+# Detect device type and set partition prefix
+detect_device_type() {
+  if [[ $DISK == *"nvme"* ]] || [[ $DISK == *"mmcblk"* ]]; then
+    PART_PREFIX="p"
+    echo ">> NVMe/MMC device detected, using partition format: ${DISK}p1"
+  else
+    PART_PREFIX=""
+    echo ">> Standard device detected, using partition format: ${DISK}1"
+  fi
+}
+
+# Detect CPU type and set microcode package
+detect_cpu_type() {
+  if grep -q "GenuineIntel" /proc/cpuinfo; then
+    UCODE="intel-ucode"
+    echo ">> Intel CPU detected, will install intel-ucode"
+  elif grep -q "AuthenticAMD" /proc/cpuinfo; then
+    UCODE="amd-ucode"
+    echo ">> AMD CPU detected, will install amd-ucode"
+  else
+    UCODE="intel-ucode"
+    echo ">> CPU type not detected, defaulting to intel-ucode"
+  fi
+}
+
+# Display the configuration to be used
+echo "Will use the following configuration:"
+echo "Disk: $DISK"
+echo "Hostname: $HOSTNAME"
+echo "Username: $USERNAME"
+echo "Swap size: $SWAP_SIZE"
+echo "Press Enter to continue or Ctrl+C to cancel..."
+read
+
+# Add error handling function
+set -e  # Exit immediately if any command fails
 error_handler() {
-    echo "错误发生在第 $1 行"
+    echo "Error occurred on line $1"
     exit 1
 }
 trap 'error_handler ${LINENO}' ERR
 
-# 配置磁盘变量
-DISK="/dev/nvme0n1"   # 请根据你的硬盘设备名称修改此变量
-
-# 在脚本开始处添加硬盘设备检查
+# Add disk device check at the beginning of the script
 if [ ! -b "$DISK" ]; then
-    echo "错误：设备 $DISK 不存在"
+    echo "Error: Device $DISK does not exist"
     exit 1
 fi
 
-# 设置时间同步
+# Detect device type and CPU type
+detect_device_type
+detect_cpu_type
+
+# Set time synchronization
 echo ">> Enabling NTP time synchronization"
 timedatectl set-ntp true
 
+# Split functionality into functions
+setup_partitions() {
+    echo ">> Partitioning disk $DISK"
+    (
+    echo g                      # Create new GPT partition table
+    echo n                      # Create new partition 1 (EFI partition)
+    echo 1                      # Partition number
+    echo                        # Default starting sector
+    echo +512m                  # Size 512m
+    echo n                      # Create new partition 2 (Swap partition)
+    echo 2                      # Partition number (changed from 3 to 2)
+    echo                        # Default starting sector
+    echo +$SWAP_SIZE            # Use command line parameter for size
+    echo n                      # Create new partition 3 (Root partition)
+    echo 3                      # Partition number (changed from 2 to 3)
+    echo                        # Default starting sector
+    echo                        # Use all remaining space
+    echo t                      # Change partition type
+    echo 1                      # EFI partition
+    echo 1                      # Select EFI system partition type
+    echo t                      # Change partition type
+    echo 2                      # Swap partition (changed from 3 to 2)
+    echo 19                     # Select Linux swap type
+    echo w                      # Write partition table and exit
+    ) | fdisk $DISK
 
+    echo ">> Formatting partitions"
+    mkfs.fat -F32 "${DISK}${PART_PREFIX}1"  # EFI partition
+    mkfs.ext4 "${DISK}${PART_PREFIX}3" -F   # Root partition (changed from 2 to 3)
+    mkswap "${DISK}${PART_PREFIX}2"         # Swap partition (changed from 3 to 2)
+    swapon "${DISK}${PART_PREFIX}2"         # Enable Swap (changed from 3 to 2)
 
-# 分区磁盘
-echo ">> Partitioning disk $DISK"
-(
-echo g                      # 创建新的 GPT 分区表
-echo n                      # 创建新的分区 1 (EFI 分区)
-echo 1                      # 分区号
-echo                        # 默认起始扇区
-echo +512m                  # 大小 512m
-echo n                      # 创建新的分区 3 (Swap 分区)
-echo 3
-echo                        # 默认起始扇区
-echo +16384M                # 大小 16GB
-echo n                      # 创建新的分区 2 (根分区)
-echo 2
-echo                        # 默认起始扇区
-echo                        # 使用剩余所有空间
-echo t                      # 修改分区类型
-echo 1                      # EFI 分区
-echo 1                      # 选择 EFI 系统分区类型
-echo t                      # 修改分区类型
-echo 3                      # Swap 分区
-echo 19                     # 选择 Linux swap 类型
-echo w                      # 写入分区表并退出
-) | fdisk $DISK
+    echo ">> Updating mirror list"
+    echo "Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/\$repo/os/\$arch" | tee /etc/pacman.d/mirrorlist
 
-# 格式化分区，使用非交互式选项
-echo ">> Formatting partitions"
-mkfs.fat -F32 "${DISK}p1"               # EFI分区，强制格式化
-mkfs.ext4 "${DISK}p2" -F                # 根分区，强制格式化
-mkswap "${DISK}p3"                      # Swap分区，强制格式化
-swapon "${DISK}p3"                      # 启用Swap
+    echo ">> Mounting partitions"
+    mount "${DISK}${PART_PREFIX}3" /mnt     
+    mkdir -p /mnt/boot && mount "${DISK}${PART_PREFIX}1" /mnt/boot
+}
 
-# 更新镜像列表
-echo ">> Updating mirror list"
-echo "Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/\$repo/os/\$arch" | tee /etc/pacman.d/mirrorlist
+install_packages() {
+    echo ">> Installing packages"
+    pacman-key --init
+    pacman-key --populate archlinux
 
-# 挂载分区
-echo ">> Mounting partitions"
-mount "${DISK}p2" /mnt
-mkdir -p /mnt/boot && mount "${DISK}p1" /mnt/boot
+    pacstrap /mnt base base-devel iptables-nft linux-lts linux-lts-headers linux-firmware vim dhcpcd git \
+        xorg xorg-server xorg-xinit alacritty rofi picom feh numlockx dunst polkit \
+        nfs-utils fastfetch pipewire pipewire-jack pipewire-alsa pipewire-pulse pavucontrol \
+        fcitx5 fcitx5-rime fcitx5-configtool rsync ntfs-3g curl p7zip ranger reflector libnotify openssh \
+        mpd mpc freerdp xf86-video-intel libva libva-intel-driver intel-media-driver vlc arp-scan unzip \
+        firefox chromium ttf-liberation wakeonlan noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji \
+        sox libva-utils telegram-desktop ufw bc firejail
 
-# 初始化 pacman 密钥环
-pacman-key --init
-# 添加 Arch Linux 官方签名密钥到本地密钥环
-pacman-key --populate archlinux
-# 安装基本系统
-echo ">> Installing base system"
-pacstrap /mnt base base-devel linux-lts linux-lts-headers linux-firmware vim dhcpcd git
+    echo ">> Generating fstab"
+    genfstab -U /mnt >> /mnt/etc/fstab
+}
+install_packages_nas() {
+    echo ">> Installing packages for NAS"
+    pacman-key --init
+    pacman-key --populate archlinux
 
-echo ">> Installing desktop environment"
-pacstrap /mnt xorg xorg-server xorg-xinit alacritty rofi picom feh numlockx dunst polkit
+    pacstrap /mnt base base-devel nfs-utils linux-lts linux-lts-headers linux-firmware vim dhcpcd git \
+    rsync openssh polkit p7zip ranger curl samba mdadm unzip ufw docker docker-compose
 
-echo ">> Installing utilities and applications"
-pacstrap /mnt nfs-utils fastfetch pipewire pipewire-jack pipewire-alsa pipewire-pulse pavucontrol fcitx5 fcitx5-rime fcitx5-configtool rsync ntfs-3g curl p7zip ranger reflector libnotify openssh
+    echo ">> Generating fstab"
+    genfstab -U /mnt >> /mnt/etc/fstab
+}
 
-echo ">> Installing multimedia and additional software"
-pacstrap /mnt mpd mpc freerdp xf86-video-intel libva libva-intel-driver intel-media-driver vlc arp-scan unzip firefox chromium ttf-liberation wakeonlan noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji sox libva-utils telegram-desktop ufw bc
+configure_system() {
+    echo ">> Configuring system"
+    ROOT_UUID=$(blkid -s UUID -o value "${DISK}${PART_PREFIX}3")  
+    arch-chroot /mnt env ROOT_UUID="$ROOT_UUID" HOSTNAME="$HOSTNAME" USERNAME="$USERNAME" UCODE="$UCODE" /bin/bash -c '
+    echo "The root UUID is: $ROOT_UUID"
+    echo "Hostname: $HOSTNAME"
+    echo "Username: $USERNAME"
+    echo "CPU Microcode: $UCODE"
+    sleep 5
 
-# 生成 fstab
-echo ">> Generating fstab"
-genfstab -U /mnt >> /mnt/etc/fstab
-# 获取根分区的 UUID
-ROOT_UUID=$(blkid -s UUID -o value "${DISK}p2")
-# 在 chroot 环境中执行命令
-echo ">> Entering chroot environment"
-arch-chroot /mnt env ROOT_UUID="$ROOT_UUID" /bin/bash -c '
-echo "The root UUID is: $ROOT_UUID"
-sleep 5
+    ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+    hwclock --systohc
 
-# 设置时区
-ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-hwclock --systohc
+    echo -e "en_US.UTF-8 UTF-8\nzh_CN.UTF-8 UTF-8" > /etc/locale.gen
+    locale-gen
+    echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
-# 配置本地化
-echo -e "en_US.UTF-8 UTF-8\nzh_CN.UTF-8 UTF-8" > /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
+    echo "$HOSTNAME" > /etc/hostname
+    echo -e "127.0.0.1       localhost\n::1             localhost\n127.0.0.1       $HOSTNAME.localdomain  $HOSTNAME" > /etc/hosts
+    echo "10.0.0.21:/mnt/user/data /home/$USERNAME/data nfs _netdev,noauto,x-systemd.automount,x-systemd.mount-timeout=10,timeo=14,x-systemd.idle-timeout=1min 0 0" >> /etc/fstab
 
-# 网络配置
-echo "huai" > /etc/hostname
-echo -e "127.0.0.1       localhost\n::1             localhost\n127.0.0.1       huai.localdomain  huai" > /etc/hosts
-echo "10.0.0.21:/mnt/user/data /home/huai/data nfs _netdev,noauto,x-systemd.automount,x-systemd.mount-timeout=10,timeo=14,x-systemd.idle-timeout=1min 0 0" >> /etc/fstab
+    echo "root:1" | chpasswd
 
-# 设置 root 密码
-echo "root:1" | chpasswd
+    pacman -S --noconfirm efibootmgr $UCODE
+    bootctl install --path=/boot
 
-# 安装引导程序和 CPU 微码
-pacman -S --noconfirm efibootmgr intel-ucode
+    echo ">> Setting up loader configuration"
+    echo "default arch.conf" > /boot/loader/loader.conf
+    echo "# timeout 5" >> /boot/loader/loader.conf
+    echo "# editor no" >> /boot/loader/loader.conf
 
-# 安装 systemd-boot
-bootctl install --path=/boot
+    echo "title   Arch Linux" > /boot/loader/entries/arch.conf
+    echo "linux   /vmlinuz-linux-lts" >> /boot/loader/entries/arch.conf
+    echo "initrd  /${UCODE}.img" >> /boot/loader/entries/arch.conf
+    echo "initrd  /initramfs-linux-lts.img" >> /boot/loader/entries/arch.conf
+    echo "options root=UUID=$ROOT_UUID rw quiet" >> /boot/loader/entries/arch.conf
 
-# 配置 loader
-echo ">> Setting up loader configuration"
-echo "default arch.conf" > /boot/loader/loader.conf
-echo "# timeout 5" >> /boot/loader/loader.conf
-echo "# editor no" >> /boot/loader/loader.conf
+    useradd -m -G wheel $USERNAME
+    echo "$USERNAME ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+    echo "$USERNAME:1" | chpasswd
 
-# 配置 systemd-boot
-echo "title   Arch Linux" > /boot/loader/entries/arch.conf
-echo "linux   /vmlinuz-linux-lts" >> /boot/loader/entries/arch.conf
-echo "initrd  /intel-ucode.img" >> /boot/loader/entries/arch.conf
-echo "initrd  /initramfs-linux-lts.img" >> /boot/loader/entries/arch.conf
-echo "options root=UUID=$ROOT_UUID rw quiet" >> /boot/loader/entries/arch.conf
+    echo -e "GTK_IM_MODULE=fcitx\nQT_IM_MODULE=fcitx\nXMODIFIERS=@im=fcitx\nSDL_IM_MODULE=fcitx\nGLFW_IM_MODULE=fcitx" >> /etc/environment
 
-# 添加新用户并设置密码
-useradd -m -G wheel huai
-echo "huai ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-echo "huai:1" | chpasswd
+    su - $USERNAME -c "cd ~ && git clone https://github.com/huai12138/linux.git && git clone https://github.com/huai12138/dwm.git && mkdir data && mkdir Pictures && mkdir Music"
 
-# 配置输入法环境变量
-echo -e "GTK_IM_MODULE=fcitx\nQT_IM_MODULE=fcitx\nXMODIFIERS=@im=fcitx\nSDL_IM_MODULE=fcitx\nGLFW_IM_MODULE=fcitx" >> /etc/environment
+    echo ">> Enabling system services"
+    systemctl enable dhcpcd
+    ufw enable
+    systemctl enable ufw 
+    '
+}
 
-# 创建必要目录
-su - huai -c "cd ~ && git clone https://github.com/huai12138/linux.git && git clone https://github.com/huai12138/dwm.git && mkdir data && mkdir Pictures && mkdir Music"
+configure_system_nas() {
+    echo ">> Configuring system for NAS"
+    ROOT_UUID=$(blkid -s UUID -o value "${DISK}${PART_PREFIX}3")  
+    arch-chroot /mnt env ROOT_UUID="$ROOT_UUID" HOSTNAME="$HOSTNAME" USERNAME="$USERNAME" UCODE="$UCODE" /bin/bash -c '
+    echo "The root UUID is: $ROOT_UUID"
+    echo "Hostname: $HOSTNAME"
+    echo "Username: $USERNAME"
+    echo "CPU Microcode: $UCODE"
+    sleep 5
 
-echo ">> Enabling system services"
-systemctl enable dhcpcd
-ufw enable
-systemctl enable ufw 
-'
+    ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+    hwclock --systohc
 
-# 在脚本结束前添加
+    echo -e "en_US.UTF-8 UTF-8" > /etc/locale.gen
+    locale-gen
+    echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+    echo "$HOSTNAME" > /etc/hostname
+    echo -e "127.0.0.1       localhost\n::1             localhost\n127.0.0.1       $HOSTNAME.localdomain  $HOSTNAME" > /etc/hosts
+    echo "root:1" | chpasswd
+
+    pacman -S --noconfirm efibootmgr $UCODE
+    bootctl install --path=/boot
+
+    echo ">> Setting up loader configuration"
+    echo "default nas.conf" > /boot/loader/loader.conf
+    echo "# timeout 5" >> /boot/loader/loader.conf
+    echo "# editor no" >> /boot/loader/loader.conf
+
+    echo "title   Nas" > /boot/loader/entries/nas.conf
+    echo "linux   /vmlinuz-linux-lts" >> /boot/loader/entries/nas.conf
+    echo "initrd  /${UCODE}.img" >> /boot/loader/entries/nas.conf
+    echo "initrd  /initramfs-linux-lts.img" >> /boot/loader/entries/nas.conf
+    echo "options root=UUID=$ROOT_UUID rw quiet" >> /boot/loader/entries/nas.conf
+
+    useradd -m -G wheel $USERNAME
+    echo "$USERNAME ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+    echo "$USERNAME:1" | chpasswd
+
+    echo ">> Enabling system services"
+    systemctl enable dhcpcd
+    systemctl enable sshd
+    '
+}
+
+# Main process flow
+setup_partitions
+
+# Choose package set based on hostname
+if [[ "$HOSTNAME" == "nas" ]]; then
+    echo ">> NAS configuration detected, installing NAS packages"
+    install_packages_nas
+    configure_system_nas
+else
+    echo ">> Desktop configuration detected, installing desktop packages"
+    install_packages
+    configure_system
+fi
+
+# Add before script ends
 echo "
 ====================================
     INSTALLATION COMPLETED!
@@ -156,10 +277,10 @@ System has been installed successfully.
 Enjoy your minimal Arch Linux system!
 ====================================
 "
-# 给用户一些时间来阅读信息
+# Give user some time to read the information
 sleep 3
 
-# 清理并重启
+# Clean up and reboot
 echo ">> Cleaning up and rebooting"
 umount -R /mnt
 reboot
