bfa1360ab1ec7965fd842232305b860ab9bf8387
huai12138 <huai12138@hotmail.com>
2025-06-05 17:33:13 +0800

    fix

--- .config/repo.sh
+++ .config/repo.sh
@@ -7,6 +7,6 @@ if ! command -v reflector &> /dev/null; then
 fi
 
 # Update mirror list
-sudo reflector -c China -a 12 -p https --score 5 --sort rate -n 3 --ipv4 --save /etc/pacman.d/mirrorlist
+sudo reflector -c China -a 12 -p https --score 5 --sort rate -n 3 --ipv4 --save /etc/pacman.d/mirrorlist &> /dev/null
 
 cat /etc/pacman.d/mirrorlist
