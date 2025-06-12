58727f194ea2e77491f7d4977d66e0c3a8e5e31b
huai12138 <huai12138@hotmail.com>
2025-06-12 17:47:54 +0800

    fix

--- nas/ufw.sh
+++ nas/ufw.sh
@@ -5,7 +5,7 @@ sudo ufw allow from 10.0.0.15 to any port 137,138 proto udp
 sudo ufw allow from 10.0.0.15 to any port 139,445 proto tcp
 sudo ufw allow from 10.0.0.8 to any port 137,138 proto udp
 sudo ufw allow from 10.0.0.8 to any port 139,445 proto tcp
-sudo ufw allow in on docker0
+sudo ufw allow in on podman0
 # HTTP and HTTPS ports
 sudo ufw allow 80 proto tcp    # HTTP
 sudo ufw allow 443 proto tcp   # HTTPS
