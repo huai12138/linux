5565cb5b8e80f6d7aa27ed224cf910f9edeadd60
huai12138 <huai12138@hotmail.com>
2025-06-16 17:58:40 +0800

    fix

--- nas/cert.sh
+++ nas/cert.sh
@@ -1,4 +1,8 @@
 curl https://get.acme.sh | sh -s email=my@example.com
 acme.sh --issue --dns dns_cf -d example.com -d *.example.com
 
-acme.sh --install-cert -d example.com --key-file /mnt/user/data/appdata/nginx/key.pem --fullchain-file /mnt/user/data/appdata/nginx/cert.pem --reloadcmd "sudo docker restart nginx-server"
+
+acme.sh --install-cert -d example.com \
+--key-file       /mnt/user/data/appdata/nginx/key.pem  \
+--fullchain-file /mnt/user/data/appdata/nginx/cert.pem \
+--reloadcmd     "sudo nginx -s reload"
