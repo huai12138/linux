b5dfe0f096145d247085a1a9d0cda072c70024f3
huai12138 <huai12138@hotmail.com>
2025-06-17 13:27:06 +0800

    fix

--- .config_x11/dwm_status.sh
+++ .config_x11/dwm_status.sh
@@ -18,7 +18,7 @@ while true; do
     time=$(date "+%a %b %d %H:%M")
     
     # 音频信息
-    volume=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}')
+    volume=$(pactl get-sink-volume @DEFAULT_SINK@ | head -n 1 | awk '{gsub(/%/, "", $5); printf "%02d%%\n", $5}')
     music=$(mpc status | awk -F"-" 'NR==1 {gsub(/^ +| +$/, "", $2); print $2}' || echo "")
     music="[$music]"
     
