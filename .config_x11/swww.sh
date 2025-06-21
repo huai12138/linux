b84c25199f4e91481906c40325640884e193967c
yljos <huai12138@hotmail.com>
2025-06-21 19:07:55 +0800

    clean

--- .config_x11/swww.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-swww img --transition-type random $(find ~/Pictures -type f | shuf | head -n 1)
-
-
--- .config_x11/swww_auto.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-while true; do
-/bin/bash /home/huai/.config/swww.sh
-sleep 180
-done
